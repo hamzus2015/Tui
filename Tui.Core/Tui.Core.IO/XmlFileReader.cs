083d997f95562d77ea4e8ed3e747b31c0439ec0a
Hamzus <hamza.essounni@gmail.com>
2018-04-17 17:25:02 +0200

    Add XmlFileReader implementation

---
+++ Tui.Core/Tui.Core.IO/XmlFileReader.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Xml;
+
+namespace Tui.Core.IO
+{
+    public class XmlFileReader
+    {
+        private string _fileName;
+
+        public XmlFileReader(string fileName)
+        {
+            this._fileName = fileName;
+        }
+
+        public string Read()
+        {
+            XmlDocument doc = new XmlDocument();
+            StringWriter strWriter = new StringWriter();
+            XmlTextWriter xmlTextWriter = new XmlTextWriter(strWriter);
+
+            doc.Load(_fileName);
+
+            doc.WriteTo(xmlTextWriter);
+
+            return strWriter.ToString();
+        }
+    }
+}
